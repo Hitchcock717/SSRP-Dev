eaedd5e27f367d05553e44747346715f99c310d6
felix_zhao <zhaobowen9876@gmail.com>
2020-05-16 14:30:30 +0800

    no message

---
+++ SSRP_spiders/ES_doc_convert.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*-coding:utf-8-*-
+'''
+    目的：论文爬取数据清洗
+    @ 此文件用于获取ES导入的json格式
+'''
+import os
+import json
+import codecs
+import pandas as pd
+
+
+class ESDocConvert(object):
+
+    # 修改csv
+    def edifycsv(self, file):
+        df = pd.read_csv(file, encoding='utf-8', header=0)
+        for col in df:
+            df[col] = df[col].astype(str).replace(',', '，')
+        df[u'date'] = df[u'date'].replace([r' \d{1,2}:\d{1,2}'], '', regex=True)
+        # print(df[u'date'].str.contains(r"(\d{4}-\d{1,2}-\d{1,2}\s\d{1,2}:\d{1,2})", regex=True))
+        df.to_csv(file, index=False, encoding='utf-8')
+
+    def convert2json(self, file, json_file):
+        self.edifycsv(file)
+        fin = codecs.open(file, 'r', encoding='utf-8')
+        info = []
+        for f in fin:
+            f_1 = f.replace('\n', '')
+            print(f_1)
+            info.append(f_1.split(','))
+        print(info)
+        fin.close()
+        fout = codecs.open(json_file, 'w+', encoding='utf-8')
+        for i in range(1, len(info)):
+            info[i] = dict(zip(info[0], info[i]))
+
+            json.dump(info[i], fout, ensure_ascii=False)  # 解决中文乱码
+            fout.write('\n')
+        fout.close()
+
+
+if __name__ == "__main__":
+    os.chdir('/Users/felix_zhao/Desktop')
+    # file = 'amino_acid.csv'
+    file = 'cqvip_data.csv'
+    doc = ESDocConvert()
+    # doc.edifycsv(file)
+    doc.convert2json(file, json_file='doc_json.txt')
